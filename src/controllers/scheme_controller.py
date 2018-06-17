fa4d40dddd8fe5a1ca4afd612c5ae65dbf1aabc4
Radomir Krawczykiewicz <kotwic4@gmail.com>
2018-06-18 01:30:46 +0200

    controllers logic separated from main

---
+++ src/controllers/scheme_controller.py
@@ -0,0 +1,26 @@
+import json
+
+from src.exceptions.invalid_usage import InvalidUsage
+from src.models.db_models import Scheme
+
+
+class SchemeController:
+
+    @staticmethod
+    def put_scheme(body):
+        new_scheme = Scheme()
+        new_scheme.scheme_json = json.dumps(body)
+        new_scheme.save()
+        return new_scheme.to_json()
+
+    @staticmethod
+    def get_scheme_info(scheme_no):
+        scheme = Scheme.select().where(Scheme.id == scheme_no).get()
+        if scheme is None:
+            raise InvalidUsage("Scheme not found", status_code=404)
+        return scheme.to_json()
+
+    @staticmethod
+    def get_schemes():
+        schemes = Scheme.select()
+        return "[" + ",".join([scheme.to_json() for scheme in schemes]) + "]"
---
+++ src/main.py
@@ -0,0 +1,91 @@
+from flask import Flask, request, jsonify, g
+from flask_cors import CORS, cross_origin
+import sqlite3
+
+from src.controllers.dataset_controller import DatasetController
+from src.controllers.model_controller import ModelController
+from src.controllers.scheme_controller import SchemeController
+from src.exceptions.invalid_usage import InvalidUsage
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+# def get_db():
+#     if not hasattr(g, 'sqlite_db'):
+#         g.sqlite_db = sqlite3.connect("db.sqlite")
+#     return g.sqlite_db
+
+@cross_origin()
+@app.route('/scheme', methods=['POST'])
+def put_scheme():
+    body = request.get_json()
+    return SchemeController.put_scheme(body)
+
+
+@cross_origin()
+@app.route('/scheme/<int:scheme_no>', methods=['GET'])
+def get_scheme_info(scheme_no):
+    return SchemeController.get_scheme_info(scheme_no)
+
+
+@cross_origin()
+@app.route('/scheme', methods=['GET'])
+def get_schemes():
+    return SchemeController.get_schemes()
+
+
+@cross_origin()
+@app.route('/model', methods=['GET'])
+def get_models():
+    return ModelController.get_models()
+
+
+@cross_origin()
+@app.route('/model', methods=['POST'])
+def train_model():
+    body = request.get_json()
+    return ModelController.train_model(body)
+
+
+@cross_origin()
+@app.route('/model/<int:model_no>', methods=['GET'])
+def get_model_info(model_no):
+    return ModelController.get_model_info(model_no)
+
+
+@cross_origin()
+@app.route('/model/<int:model_no>/file/<filename>', methods=['GET'])
+def get_trained_model(model_no, filename):
+    return ModelController.get_trained_model(model_no, filename)
+
+
+@cross_origin()
+@app.route('/data')
+def get_datasets():
+    return DatasetController.get_datasets()
+
+
+@app.route('/data/<dataset_id>/bitmaps/<int:image_no>')
+def get_bitmap(dataset_id, image_no):
+    return DatasetController.get_bitmap(dataset_id, image_no)
+
+
+@app.route('/data/<int:dataset_id>/')
+def get_dataset_info(dataset_id):
+    return DatasetController.get_dataset_info(dataset_id)
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
