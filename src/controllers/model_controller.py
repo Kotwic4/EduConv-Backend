525eeaa833d687d4e03d768037d0d3efda23df71
albert <albert.mosialek@gmail.com>
2018-10-29 19:54:17 +0100

    POST on /model now returns correct value

--- src/controllers/model_controller.py
+++ src/controllers/model_controller.py
@@ -3,21 +3,21 @@ import json
 from flask import jsonify
 
 from src.exceptions.invalid_usage import InvalidUsage
-from src.models.db_models import Model
+from src.models.db_models import NNModel
 
 
 class ModelController:
 
     @staticmethod
     def _get_model(model_no):
-        model = Model.select().where(Model.id == model_no).get()
+        model = NNModel.select().where(NNModel.id == model_no).get()
         if model is None:
             raise InvalidUsage("model not found", status_code=404)
         return model
 
     @staticmethod
     def put_model(body):
-        new_model = Model()
+        new_model = NNModel()
         new_model.model_json = json.dumps(body["model_json"])
         new_model.name = body.get("name")
         new_model.save()
@@ -30,7 +30,7 @@ class ModelController:
 
     @staticmethod
     def get_models():
-        models = Model.select()
+        models = NNModel.select()
         return jsonify([model.to_dict() for model in models])
 
     @staticmethod
