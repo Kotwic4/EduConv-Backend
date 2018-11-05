17316cb5b61f9054b45c4bab8653858fe1301335
Radomir Krawczykiewicz <radomir.krawczykiewicz@swmansion.com>
2018-11-05 10:52:51 +0100

    add learning statistics

--- src/controllers/trained_model_controller.py
+++ src/controllers/trained_model_controller.py
@@ -3,13 +3,12 @@ from flask import send_from_directory, jsonify
 from src.datasets.datasets_map import check_if_dataset_class_exists
 from src.exceptions.invalid_usage import InvalidUsage
 from src.models.db_models import NNModel, NNTrainedModel, Dataset, ModelsQueue
-from src.train.keras_model_creator import KerasModelBuilder
 
 
-class trained_ModelController:
+class TrainedModelController:
 
     @staticmethod
-    def _trained_model_path(trained_model):
+    def trained_model_path(trained_model):
         return "db/trained_models/" + str(trained_model.get_id())
 
     @staticmethod
@@ -31,7 +30,6 @@ class trained_ModelController:
             raise InvalidUsage("trained_model not found", status_code=404)
         return trained_model
 
-
     @staticmethod
     def train_trained_model(body):
         if "dataset" not in body.keys():
@@ -39,22 +37,22 @@ class trained_ModelController:
 
         model_id = body["model_id"]
         dataset_name = body["dataset"]
-        name = body.get("name") #None if not found in json
+        name = body.get("name")  # None if not found in json
         params = body["params"]
         model = NNModel.select().where(NNModel.id == model_id).get()
 
         dataset = Dataset.select().where(Dataset.name == dataset_name).get()
-        dataset_class = check_if_dataset_class_exists(dataset_name)
+        check_if_dataset_class_exists(dataset_name)
 
-        trained_model = trained_ModelController._create_trained_model(model, dataset, params, name)
+        trained_model = TrainedModelController._create_trained_model(model, dataset, params, name)
         mq = ModelsQueue()
-        mq.model_to_be_trained=trained_model
+        mq.model_to_be_trained = trained_model
         mq.save()
         return jsonify(trained_model.to_dict())
 
     @staticmethod
     def get_trained_model_info(trained_model_no):
-        return jsonify(trained_ModelController._get_trained_model(trained_model_no).to_dict())
+        return jsonify(TrainedModelController._get_trained_model(trained_model_no).to_dict())
 
     @staticmethod
     def get_trained_models():
@@ -63,11 +61,11 @@ class trained_ModelController:
 
     @staticmethod
     def get_trained_trained_model(trained_model_no, filename):
-        trained_model = trained_ModelController._get_trained_model(trained_model_no)
-        dir_path = trained_ModelController._trained_model_path(trained_model)
+        trained_model = TrainedModelController._get_trained_model(trained_model_no)
+        dir_path = TrainedModelController.trained_model_path(trained_model)
         return send_from_directory(dir_path, filename)
 
     @staticmethod
     def delete_trained_model(trained_model_no):
-        trained_model = trained_ModelController._get_trained_model(trained_model_no)
+        trained_model = TrainedModelController._get_trained_model(trained_model_no)
         trained_model.delete_instance()
--- src/models/db_models.py
+++ src/models/db_models.py
@@ -17,13 +17,37 @@ class BaseModel(Model):
 
 
 class Dataset(BaseModel):
-    img_depth = IntegerField(null=True)
-    img_height = IntegerField(null=True)
-    img_width = IntegerField(null=True)
-    labels = TextField(null=True)
-    name = TextField(null=True, unique=True)
-    test_images_count = IntegerField(null=True)
-    train_images_count = IntegerField(null=True)
+    img_depth = IntegerField(column_name='img_depth',
+                             constraints=[Check('img_depth > 0')],
+                             default=1,
+                             unique=False,
+                             null=False)
+    img_height = IntegerField(column_name='img_height',
+                              constraints=[Check('img_height > 0')],
+                              default=1,
+                              unique=False,
+                              null=False)
+    img_width = IntegerField(column_name='img_width',
+                             constraints=[Check('img_width > 0')],
+                             default=1,
+                             unique=False,
+                             null=False)
+    labels = TextField(column_name='labels',
+                       unique=False,
+                       null=False)
+    name = TextField(column_name='name',
+                     unique=True,
+                     null=False)
+    test_images_count = IntegerField(column_name='test_images_count',
+                                     constraints=[Check('test_images_count >= 0')],
+                                     default=0,
+                                     unique=False,
+                                     null=False)
+    train_images_count = IntegerField(column_name='train_images_count',
+                                      constraints=[Check('train_images_count >= 0')],
+                                      default=0,
+                                      unique=False,
+                                      null=False)
 
     def to_dict(self):
         return {
@@ -50,19 +74,42 @@ class NNModel(BaseModel):
 
     def to_dict(self):
         return {
-            "id": self.get_id(), 
-            "model_json": json.loads(self.model_json), 
-            "name":self.name
-            }
+            "id": self.get_id(),
+            "model_json": json.loads(self.model_json),
+            "name": self.name
+        }
 
 
 class NNTrainedModel(BaseModel):
-    dataset = ForeignKeyField(column_name='dataset_id', field='id', model=Dataset, null=True)
-    epochs_learnt = IntegerField(constraints=[SQL("DEFAULT 0")], null=True)
-    epochs_to_learn = IntegerField(constraints=[SQL("DEFAULT 0")], null=True)
-    model = ForeignKeyField(column_name='scheme_id', field='id', model=NNModel, null=True)
-    name = TextField(null=True)
-    batch_size = IntegerField(column_name='batch_size')
+    dataset = ForeignKeyField(column_name='dataset_id',
+                              field='id',
+                              model=Dataset,
+                              null=False,
+                              unique=False)
+    epochs_learnt = IntegerField(column_name='epochs_learnt',
+                                 constraints=[Check('epochs_learnt >= 0')],
+                                 default=0,
+                                 unique=False,
+                                 null=False)
+    epochs_to_learn = IntegerField(column_name='epochs_to_learn',
+                                   constraints=[Check('epochs_to_learn > 0')],
+                                   default=1,
+                                   unique=False,
+                                   null=False)
+    model = ForeignKeyField(column_name='model_id',
+                            field='id',
+                            model=NNModel,
+                            unique=False,
+                            null=False)
+    name = TextField(column_name='name',
+                     unique=False,
+                     null=True)
+    batch_size = IntegerField(column_name='batch_size',
+                              constraints=[Check('batch_size > 0')],
+                              default=1,
+                              unique=False,
+                              null=False)
+
     def to_dict(self):
         return {
             "id": self.get_id(),
@@ -70,13 +117,61 @@ class NNTrainedModel(BaseModel):
             "epochs_learnt": self.epochs_learnt,
             "epochs_to_learn": self.epochs_to_learn,
             "batch_size": self.batch_size,
-            "name": self.name
+            "name": self.name,
+            "model_id": self.model.id,
+            "epochs_data": self.epochs_data()
+        }
+
+    def epochs_data(self):
+        epoch_data_list = list(ModelEpochData
+                               .select()
+                               .where(ModelEpochData.model == self))
+        epoch_data_dict = {}
+        for x in epoch_data_list:
+            epoch_data_dict[x.epoch_number] = x.to_dict()
+        return epoch_data_dict
+
+    class Meta:
+        table_name = 'trained_models'
+
+
+class ModelEpochData(BaseModel):
+    model = ForeignKeyField(column_name='trained_model_id',
+                            field='id',
+                            model=NNTrainedModel,
+                            unique=False,
+                            null=False)
+    epoch_number = IntegerField(column_name='epoch_number',
+                                constraints=[Check('epoch_number >= 0')],
+                                unique=False,
+                                null=False)
+    acc = DoubleField(column_name='acc',
+                      constraints=[Check('acc >= 0')],
+                      unique=False,
+                      null=False)
+    loss = DoubleField(column_name='loss',
+                       constraints=[Check('loss >= 0')],
+                       unique=False,
+                       null=False)
+
+    def to_dict(self):
+        return {
+            "acc": self.acc,
+            "loss": self.loss
         }
 
     class Meta:
-        table_name = 'trainedModels'
+        table_name = 'model_epoch_data'
+        primary_key = False
+        constraints = [SQL('UNIQUE(trained_model_id, epoch_number)')]
+
 
 class ModelsQueue(BaseModel):
-    model_to_be_trained = ForeignKeyField(column_name='trained_model_id', field='id', model=NNTrainedModel, null=False)
+    model_to_be_trained = ForeignKeyField(column_name='trained_model_id',
+                                          field='id',
+                                          model=NNTrainedModel,
+                                          unique=True,
+                                          null=False)
+
     class Meta:
-        table_name = 'ModelsQueue'
+        table_name = 'models_queue'
