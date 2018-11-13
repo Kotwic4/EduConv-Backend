bb56e0b949a1f52d9256883f2eab32179c5e72c5
albert <albert.mosialek@gmail.com>
2018-11-13 20:32:32 +0100

    change codestyle

--- init_db.py
+++ init_db.py
@@ -38,52 +38,50 @@ def add_mnist():
     mnist.name = "mnist"
     mnist.train_images_count = 60000
     mnist.test_images_count = 10000
- 
-
     mnist.save()
 
+
 def recreate_images_and_labels(dataset_class, shape_of_image, image_type):
-    dataset=dataset_class()
+    dataset = dataset_class()
     ensure_directory(f"db/datasets/{dataset.name}/train/")
     ensure_directory(f"db/datasets/{dataset.name}/test/")
     print(f'adding {dataset.name} labels')
-    add_labels(dataset.y_train,dataset_class.get_labels(),f"db/datasets/{dataset.name}/train/")
-    add_labels(dataset.y_test,dataset_class.get_labels(),f"db/datasets/{dataset.name}/test/")
+    add_labels(dataset.y_train, dataset_class.get_labels(), f"db/datasets/{dataset.name}/train/")
+    add_labels(dataset.y_test, dataset_class.get_labels(), f"db/datasets/{dataset.name}/test/")
     data = dataset.x_train
     print(f'adding {dataset.name} train bitmaps')
-    add_bitmaps(data,f"db/datasets/{dataset.name}/train/",shape_of_image,image_type)
+    add_bitmaps(data, f"db/datasets/{dataset.name}/train/", shape_of_image, image_type)
     data = dataset.x_test
     print(f'adding {dataset.name} test bitmaps')
-    add_bitmaps(data,f"db/datasets/{dataset.name}/test/",shape_of_image,image_type)
-    
-    
+    add_bitmaps(data, f"db/datasets/{dataset.name}/test/", shape_of_image, image_type)
+
+
 def add_bitmaps(data, dataset_path, shape_of_image, image_type):
     image_array = data.reshape([data.shape[0]]+shape_of_image)
     for index, image in enumerate(image_array):
-        with open(str.format(path.join(dataset_path,"{}.bmp"), index), "wb+") as f:
-            img = Image.fromarray((image * 255).astype('uint8'),image_type).save(f, 'bmp')
-            
+        with open(str.format(path.join(dataset_path, "{}.bmp"), index), "wb+") as f:
+            img = Image.fromarray((image * 255).astype('uint8'), image_type).save(f, 'bmp')
 
-def add_labels(data,labels, dataset_path):
-    with open(str.format(path.join(dataset_path,"labels.txt")), "w+") as f:
+
+def add_labels(data, labels, dataset_path):
+    with open(str.format(path.join(dataset_path, "labels.txt")), "w+") as f:
         f.write(" ".join([str(labels[label.tolist().index(max(label))]) for label in data]))
-
 
 
 def add_cifar():
     print('adding cifar')
     db = sqlite3.connect("db/db.sqlite")
-    Cifar10Input.acquire(db,recreate_images=recreate_images_and_labels)
+    Cifar10Input.acquire(db, recreate_images=recreate_images_and_labels)
     db.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--db',action='store_true')
-    parser.add_argument('--mnist',action='store_true')
-    parser.add_argument('--cifar10',action='store_true')
-    parser.add_argument('--images',action='store_true')
-    parser.add_argument('--all',action='store_true')
+    parser.add_argument('--db', action='store_true')
+    parser.add_argument('--mnist', action='store_true')
+    parser.add_argument('--cifar10', action='store_true')
+    parser.add_argument('--images', action='store_true')
+    parser.add_argument('--all', action='store_true')
     args = parser.parse_args()
     if args.all:
         args.db = args.mnist = args.cifar10 = args.images = True
@@ -92,8 +90,8 @@ if __name__ == "__main__":
     if args.mnist:
         add_mnist()
         if args.images:
-            recreate_images_and_labels(MnistInput,[28,28],'L')
+            recreate_images_and_labels(MnistInput, [28, 28], 'L')
     if args.cifar10:
         add_cifar()
         if args.images:
-            recreate_images_and_labels(Cifar10Input,[32,32,3],'RGB')
+            recreate_images_and_labels(Cifar10Input, [32, 32, 3], 'RGB')
